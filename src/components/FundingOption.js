2f8718b03f067683b01b4a06bf028e59229d531a
Joshua <30259508@cityofglacol.ac.uk>
2020-11-30 20:15:20 +0000

    Replaced placeholder images in Funding section with companie Logos using Gatsby Image and fixed page styling (Finished Funding Page)

---
+++ src/components/FundingOption.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import Image from 'gatsby-image'
+
+const SetImg = styled(Image)`
+  margin: auto;
+  flex: 1 1 150px;
+  margin-right: 20px; /* Spacing between logo and content */
+
+  /* Hides funding option logos on screens under 600px for maximum image legibility */
+  @media screen and (max-width: 600px) {
+      display: none;
+  }
+`
+
+const StyledDiv = styled.div`
+  display: flex;
+  padding-bottom: 50px;
+
+  /* Overides default margins (26px) as it was asthethically clunky on the Funding page */
+  h2 {
+    margin-bottom: 15px;
+  }
+  p {
+    margin-bottom: 10px;
+  }
+
+
+  /* Content will try to take up 80% of screen size minimum and will grow/shrink accordingly */
+  .content {
+    flex: 1 1 80%;
+  }
+`
+
+const FundingOption = ({ name, content, link, image }) => {
+  return (
+    <StyledDiv>
+      <SetImg fluid={image} />
+      <div class='content'>
+        <h2>{name}</h2>
+        <p>{content}</p>
+        <a href={link}>{link}</a>
+      </div>
+    </StyledDiv>
+  )
+}
+
+export default FundingOption;
--- src/pages/funding.js
+++ src/pages/funding.js
@@ -1,13 +1,61 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import FundingOption from '../components/FundingOption'
+import { graphql } from 'gatsby'
+import Image from 'gatsby-image'
 
-const FundingPage = () => (
+const FundingPage = ({ data }) => (
   <Layout>
     <SEO title="Funding" />
     <h1>Funding Options</h1>
     <p>Nostrud irure sunt do Lorem aliqua elit ut labore dolore velit esse cupidatat. Ex sit eiusmod tempor non eu tempor veniam occaecat laboris quis ut mollit magna ex. Reprehenderit ad consectetur anim id deserunt dolore ipsum aliquip amet. Eiusmod mollit incididunt ullamco nostrud. Dolore do tempor veniam voluptate esse nisi esse sit consectetur officia nostrud consectetur enim in. Non fugiat incididunt voluptate qui id ex nostrud est aliqua reprehenderit laboris ullamco. Veniam occaecat ipsum ad ea in laboris nulla incididunt velit nisi quis ullamco proident labore.</p>
+
+    <FundingOption 
+      name={"The Scholarship Hub"}
+      content={"The Scholarship Hub is a social enterprise both providing information on scholarships and developing new scholarships."} 
+      link={"https://www.thescholarshiphub.org.uk"}
+      image={data.scholarshipHubGatsbyImage.childImageSharp.fluid}
+    />
+    <FundingOption 
+      name={"Turn2Us"}
+      content={"Turn2Us is a national charity helping people when times are tough. They provide information and financial support to those in need. They can be a great resource for potential funding and grants you may be edible for well as great general budgeting advice."} 
+      link={"https://www.turn2us.org.uk"}
+      image={data.turn2UsGatsbyImage.childImageSharp.fluid}
+    />
+    <FundingOption 
+      name={"UK Government Website - Student Finance"}
+      content={"The UK government’s website is a fantastic resource for information regarding your funding your studies and further education. The website is remarkably easy to navigate and with the government’s seal of approval you can guarantee accuracy of information more so than many other portals."} 
+      link={"https://www.gov.uk/browse/education/student-finance"}
+      image={data.govUkGatsbyImage.childImageSharp.fluid}
+    />
   </Layout>
 )
 
 export default FundingPage
+
+export const query = graphql`
+  query {
+    govUkGatsbyImage: file(relativePath: {eq: "gov-uk.png"}) {
+      childImageSharp {
+        fluid(maxWidth: 5500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    scholarshipHubGatsbyImage: file(relativePath: {eq: "the-scholarship-hub.png"}) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    turn2UsGatsbyImage: file(relativePath: {eq: "turn-2-us.png"}) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
