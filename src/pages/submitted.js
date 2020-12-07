5f35363e421be8dbe8123af56917333812021193
Joshua <30259508@cityofglacol.ac.uk>
2020-12-07 13:42:14 +0000

    Finished Form submission page

--- src/pages/submitted.js
+++ src/pages/submitted.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
 import styled from 'styled-components'
+import MessageReceived from '../images/message-received.svg'
+import Animate, { FadeIn } from 'animate-css-styled-components'
+import StyledLink from '../components/Navigation/StyledLink'
+
 
 const StyledDiv = styled.div`
   display: flex;
@@ -8,15 +12,42 @@ const StyledDiv = styled.div`
   justify-content: center;
   align-items: center;
   text-align: center;
+  margin-bottom: 20px;
+
+  a {
+    width: 70%;
+  }
+`
+
+const StyledAnimate = styled(Animate)`
+  flex: 1;
+  max-width: 45%;
+  align-self: center;
+
+  /* Hide illustration on smaller screensizes */
+  @media only screen and (max-width: 900px) {
+    max-width: 80%;
+  }
 `
 
 export const SubmittedPage = () => {
   return (
     <Layout>
       <StyledDiv>
-        <h3>Thank you for your message</h3>
-        <p>We aim to get back to you soon!</p>
+        <h3>Message received 👍</h3>
+
+        <StyledAnimate 
+          Animation={[FadeIn]}
+          duration={['2s']}
+          delay={['0s']}
+        >  
+          <img src={MessageReceived} />
+        </ StyledAnimate>
+        <h4>Thank you for your message</h4>
+        <p>We endevour to get back to you as soon as possible!</p>
+        <StyledLink to='/' name='Return to Homepage' />
       </StyledDiv>
+
     </Layout>
   )
 }
