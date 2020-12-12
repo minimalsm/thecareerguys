52e116f6294864a83682e18db99e018680ae07fc
Joshua <30259508@cityofglacol.ac.uk>
2020-12-12 21:08:06 +0000

    Remove unused graphql query from about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,13 +16,6 @@ export default AboutPage
 
 export const query = graphql`
   query {
-    typingGatsbyImage: file(relativePath: {eq: "typing-macbook.jpg"}) {
-      childImageSharp {
-        fluid(maxWidth: 1500) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
     joshuaGatsbyImage: file(relativePath: {eq: "joshua-douglas.png"}) {
       childImageSharp {
         fluid(maxWidth: 500) {
